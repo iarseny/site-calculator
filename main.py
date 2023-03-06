3b1ed669cbc45760b3f6087050d7f8a0e0b32ba2
iarseny <122807698+iarseny@users.noreply.github.com>
2023-03-06 20:53:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+number1 = None
+number2 = None
+number3 = None
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/count", methods=["POST"])
+def count():
+    print(request.form)
+    return jsonify({1: 123})
+
+if __name__ == "__main__":
+    app.run()
